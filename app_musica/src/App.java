d1183941296ed26274a945bda7e54a17d8e3a94d
ingrid <ingrid.pereira2@estudante.ifto.edu.br>
2022-12-15 10:21:46 -0300

    Adicinando funções

--- app_musica/src/App.java
+++ app_musica/src/App.java
@@ -11,21 +11,60 @@ public class App {
         Musica mus = new Musica();
         Genero gen = new Genero();
         Album alb = new Album(); 
+        int escolha = 0; 
 
 
         
         System.out.println("Informe seu nome para fazer o cadastro:");
         usu.setNome(sc.nextLine());
 
+        sc.nextLine();
         System.out.print("\n Informe sua idade:");
         usu.setIdade(sc.nextInt());
 
+        while (escolha  != 6){
+        System.out.print("\n Digite: \n 1 - escutar musica \n 2- criar playlist  \n 3 - Escutar um album    \n 4 - Assinar plano artistista\n 5 - ouvir playlist de generos \n 6 - Sair ");
+        escolha= sc.nextInt();
+
+        if (escolha == 1){
+        }
+
+        else if (escolha == 2){
+        }
+        
+        else if (escolha == 3){
+        }
+
+        else if (escolha == 4){
+            System.out.println("Informe sua gravadora:");
+            art.setGravadora(sc.nextLine());
+            
+        } 
+        else if (escolha == 5){
+            System.out.println("qual genero gostaria de ouvir:");
+            while (escolha  != 3){
+                System.out.print("\n 1 - Blues \n 2- pop  \n 3 - nenhum");
+                escolha= sc.nextInt(); 
+                
+        if (escolha == 1){ 
+
+        } 
+        else if (escolha == 2){
+        }
+        else if (escolha == 3){
+        }
+        
+           
+        }
         
 
 
-
         
+
 
 
     }
+
+    }
+}
 }
